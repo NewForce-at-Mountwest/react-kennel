4d06590a2d9ff15299d652dc5345262e228f565f
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-03-07 14:43:59 -0500

    fixed bug! Thanks michelle!

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
 class AnimalList extends Component {
 
-  animalsWithOwners = this.props.animals.map(singleAnimal => {
-    const matchingAnimalOwnerEntries = this.props.animalOwners.filter(
-      singleAnimalOwnerEntry => {
-        return singleAnimalOwnerEntry.animalId === singleAnimal.id;
-      }
-    );
-
-    const matchingOwnerObjects = matchingAnimalOwnerEntries.map(
-      joinTableEntry => {
-        const matchingOwner = this.props.owners.find(singleOwner => {
-          return singleOwner.id === joinTableEntry.ownerId;
-        });
-        return matchingOwner;
-      }
-    );
-
-    return {
-      id: singleAnimal.id,
-      name: singleAnimal.name,
-      owners: matchingOwnerObjects
-    };
-  });
-
   render() {
+    const animalsWithOwners = this.props.animals.map(singleAnimal => {
+      const matchingAnimalOwnerEntries = this.props.animalOwners.filter(
+        singleAnimalOwnerEntry => {
+          return singleAnimalOwnerEntry.animalId === singleAnimal.id;
+        }
+      );
+
+      const matchingOwnerObjects = matchingAnimalOwnerEntries.map(
+        joinTableEntry => {
+          const matchingOwner = this.props.owners.find(singleOwner => {
+            return singleOwner.id === joinTableEntry.ownerId;
+          });
+          return matchingOwner;
+        }
+      );
+
+      return {
+        id: singleAnimal.id,
+        name: singleAnimal.name,
+        owners: matchingOwnerObjects
+      };
+    });
+
+
+
     return (
       <article>
         <h1>Animals</h1>
-        {this.animalsWithOwners.map(animal => {
+        {animalsWithOwners.map(animal => {
           return (
             <div key={animal.id}>
               <h3>{animal.name}</h3>
